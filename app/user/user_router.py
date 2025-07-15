327d75843d0869027387654030f97f380b1f7571
isolatedOctane <smilebear0830@gmail.com>
2025-07-15 19:28:13 +0900

    implement user API endpoints (login, register, delete, update-password)

--- app/user/user_router.py
+++ app/user/user_router.py
@@ -19,16 +19,46 @@ def login_user(user_login: UserLogin, service: UserService = Depends(get_user_se
 @user.post("/register", response_model=BaseResponse[User], status_code=status.HTTP_201_CREATED)
 def register_user(user: User, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    """
+    register_user로 가입 시도
+    성공시 201 created
+    실패 시(400 이메일 이미 존재) 400 error 
+    """
+    try:
+        registered_user=service.register_user(user)
+        return BaseResponse(status="success", data=registered_user, message="User registeration success.")
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
 
 
 @user.delete("/delete", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def delete_user(user_delete_request: UserDeleteRequest, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    """
+    delete_user로 삭제 시도
+    실패 시 Error 404, 출력 구문은 에러의 종류에 따라 다름
+
+    """
+    try:
+        deleted_user=service.delete_user(user_delete_request.email)
+        return BaseResponse(status="success", data=deleted_user, message="User Deletion Success.")
+        
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
 
 
 @user.put("/update-password", response_model=BaseResponse[User], status_code=status.HTTP_200_OK)
 def update_user_password(user_update: UserUpdate, service: UserService = Depends(get_user_service)) -> BaseResponse[User]:
     ## TODO
-    return None
+    """
+    user_update_pwd로 패스워드 업데이트 시도
+    실패 시 404 Error
+    """
+    try:
+        updated_user=service.update_user_pwd(user_update)
+        return BaseResponse(status="success", data=updated_user, message="User password update success.")
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+
